c5d218bc1689cb763e565070e5d43240941493e3
Peih Brown <peih@offspringdigital.com>
2024-12-01 05:40:15 +0700

    update env

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,8 +3,10 @@ import axios from "axios"
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
   headers: {
-    "Content-Type": "application/json",
-  },
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
 })
 
 // Request interceptor
