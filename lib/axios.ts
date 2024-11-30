48c73c87bb72deb8e2ab62067690e71cd3b63107
hungvuong11 <hungvuong@technixo.com>
2024-11-30 20:32:19 +0700

    update config api

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,8 +2,10 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 })
 
