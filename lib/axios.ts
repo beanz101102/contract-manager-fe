84f00e24c1dbe0d6229a8bba4593279c74117b33
Peih Brown <peih@offspringdigital.com>
2024-12-01 12:53:37 +0700

    remove origin *

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,11 +2,9 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*'
   }
 })
 
